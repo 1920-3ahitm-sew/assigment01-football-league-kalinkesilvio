97a617591ce7fd1591b3a3b17813a0c033ed9e01
kalinkesilvio <s.kalinke@students.htl-leonding.ac.atgit config --global user.email s.kalinke@students.htl-leonding.ac.at>
2019-09-19 18:52:35 +0200

    first commmit

--- src/main/java/at/htl/football/Match.java
+++ src/main/java/at/htl/football/Match.java
@@ -1,4 +1,40 @@
 package at.htl.football;
 
 public class Match {
+
+    private String homeTeam;
+    private String guestTeam;
+    private int homeGoals;
+    private int guestGoals;
+
+    public Match(String homeTeam, String guestTeam, int homeGoals, int guestGoals) {
+        this.homeTeam = homeTeam;
+        this.guestTeam = guestTeam;
+        this.homeGoals = homeGoals;
+        this.guestGoals = guestGoals;
+    }
+
+    public String getHomeTeam() {
+        return homeTeam;
+    }
+
+    public String getGuestTeam() {
+        return guestTeam;
+    }
+
+    public int getHomeGoals() {
+        return homeGoals;
+    }
+
+    public int getGuestGoals() {
+        return guestGoals;
+    }
+
+    public int getHomePoints() {
+        return 0;
+    }
+
+    public int getGuestPoints() {
+        return 0;
+    }
 }
